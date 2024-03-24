f87e0eb440d26daf1703cf4ee082e942d87e1e47
unknown <sharansk792000@gmail.com>
2024-03-24 23:53:07 +0530

    updated

--- diceroll/lib/main.dart
+++ diceroll/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    const MaterialApp(
-      home: Scaffold,
-    )
-  )
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 177, 91, 230),
+      body: Center(
+        child: Text("data"),
+      ),
+    ),
+  ));
 }
